37aef2e9ebfde315b7fef4cde90a890603945662
RomanShilo <shilo-r2013@yandex.ru>
2024-12-25 14:24:29 +0300

    Создание js файла

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        "name": "html",
+        "level": "40"
+    },
+    {
+        "name": "css",
+        "level": "20"
+    },
+    {
+        "name": "python",
+        "level": "60"
+    },
+    {
+        "name": "cpp",
+        "level": "20"
+    }
+]
+
+const skillList = document.querySelector('dl.skill-list');
